3c9e110b60106160f0b1a0607257b9b5d92b42a5
johankiilavirta <51455791+johankiilavirta@users.noreply.github.com>
2024-05-13 14:45:57 -0400

    Update affine_transform.py
    Border reflections

--- affine_transform.py
+++ affine_transform.py
@@ -6,7 +6,7 @@ def translate_image(image, x_offset, y_offset):
         [1, 0, x_offset],
         [0, 1, y_offset]
     ])
-    result = cv2.warpAffine(image, translation_matrix, (image.shape[1], image.shape[0]), borderMode=cv2.BORDER_REPLICATE)
+    result = cv2.warpAffine(image, translation_matrix, (image.shape[1], image.shape[0]), borderMode=cv2.BORDER_REFLECT)
     return result
 
 def rotate_image(image, theta_degrees):
@@ -14,5 +14,5 @@ def rotate_image(image, theta_degrees):
     center = (image.shape[1]//2, image.shape[0]//2)
     rot_mat = cv2.getRotationMatrix2D(center, theta_degrees, 1)
     
-    result = cv2.warpAffine(image, rot_mat, (image.shape[1], image.shape[0]), borderMode=cv2.BORDER_REPLICATE)
+    result = cv2.warpAffine(image, rot_mat, (image.shape[1], image.shape[0]), borderMode=cv2.BORDER_REFLECT)
     return result
